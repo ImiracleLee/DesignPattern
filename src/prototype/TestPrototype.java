e0d7d39d26280ed22383198eff848f1004ea4075
ImiracleLee <786061326@qq.com>
2017-09-08 02:01:24 +0800

    prototype and template method

---
+++ src/prototype/TestPrototype.java
@@ -0,0 +1,22 @@
+package prototype;
+
+/**
+ * Created by Imiracle Lee on 2017/9/7.
+ */
+public class TestPrototype {
+    public static void main(String[] args) {
+        Resume a = new Resume("Name");
+        a.setPersonalInfo("M", "24");
+        a.setWorkExperience("2012-2016", "XX company");
+
+        Resume b = (Resume) a.clone();
+        b.setWorkExperience("2016-2017", "YY company");
+
+        Resume c = (Resume) a.clone();
+        c.setPersonalInfo("M", "25");
+
+        a.display();
+        b.display();
+        c.display();
+    }
+}
---
+++ src/template/TestPaperA.java
@@ -0,0 +1,16 @@
+package template;
+
+/**
+ * Created by Imiracle Lee on 2017/9/7.
+ */
+public class TestPaperA extends TestPaper {
+    @Override
+    protected String answer1() {
+        return "a";
+    }
+
+    @Override
+    protected String answer2() {
+        return "b";
+    }
+}
